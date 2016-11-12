1433fd673d585160ec0adc8b65b78d4249ae845e
Axel Nowaczyk <axelnowaczyk@gmail.com>
2016-11-12 17:55:23 +0100

    ScrollItemView added

---
+++ EventBuddy/Views/ScrollItemView.swift
@@ -0,0 +1,33 @@
+//
+//  ScrollItemView.swift
+//  EventBuddy
+//
+//  Created by Axel Nowaczyk on 12.11.2016.
+//  Copyright © 2016 Axel Nowaczyk. All rights reserved.
+//
+
+import UIKit
+
+class ScrollItemView: UIView {
+
+    @IBOutlet var pictureImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    
+    class func instanceFromNib() -> UIView {
+        return UINib(nibName: "ScrollItemView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! UIView
+    }
+    
+    class func scrollItemView(model: SingularObjectModel) -> ScrollItemView {
+        
+        let scrollItem = ScrollItemView.instanceFromNib() as! ScrollItemView
+        
+        scrollItem.pictureImageView.image = UIImage(named: model.image ?? "")
+        scrollItem.nameLabel.text = model.name
+        scrollItem.addressLabel.text = model.address
+        
+        return scrollItem
+        
+    }
+
+}

75e7e2775c8eb306044f04ea80fcff61d40f515e
Axel Nowaczyk <axelnowaczyk@gmail.com>
2016-11-29 19:52:13 +0100

    added search bar, added keywords in sigular model

---
+++ EventBuddy/Extensions/Realm+String.swift
@@ -0,0 +1,16 @@
+//
+//  Realm+String.swift
+//  EventBuddy
+//
+//  Created by Axel Nowaczyk on 25.11.2016.
+//  Copyright © 2016 Axel Nowaczyk. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RLMString: Object {
+    
+    dynamic var value = ""
+    
+}

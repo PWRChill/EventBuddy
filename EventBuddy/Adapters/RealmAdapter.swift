536f94304cd76bbf03e86eb0638378f3ad20e1c9
Rafał Wycichowski <Wyci@rafas-macbook-pro.local>
2016-11-29 23:08:26 +0100

    Added  databases vol. 2

--- EventBuddy/Adapters/RealmAdapter.swift
+++ EventBuddy/Adapters/RealmAdapter.swift
@@ -103,8 +103,31 @@ class RealmAdapter {
         RealmAdapter.save(model: entertainments)
         
         let sports = GroupModel.groupModel(name: "Sport", models: nil)
-        sports.add(model: SingularObjectModel.singularObjectModel(name: "Running", image: nil, address: nil, desc: nil, lat: 0, lon: 0, rank: 0))
-        sports.add(model: SingularObjectModel.singularObjectModel(name: "Swimming pool", image: nil, address: nil, desc: nil, lat: 0, lon: 0, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Jatomi Fitness", image: "JatomiFitness", address: "plac Grunwaldzki 22, 50-363 Wrocław", desc: "gym", lat: 51.112686, lon: 17.059022, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Klub sportowy Forma", image: "KlubSportowyForma", address: "Wygodna 1, 50-323 Wrocław", desc: "gym", lat: 51.122728, lon: 17.047582, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Fitness Błękitna Laguna", image: "FitnessBlekitnaLaguna", address: "Słowiańska 39, 48-300 Wrocław", desc: "gym", lat: 51.123686, lon: 17.043966, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Centrum Treningowe Spartan WCT4 Wrocław", image: "CentrumTreningoweSpartan", address: "Trzebnicka 33, 50-231 Wrocław", desc: "gym", lat: 51.127296, lon: 17.036427, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Fitness World - Wrocław, Psie Pole", image: "FitnessWorldWrocławPsiePole", address: "Jana Długosza 34, 51-162 Wrocław", desc: "gym", lat: 51.133288, lon: 17.062001, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Klub Bodyfit Fitness - Centrum Śródmieście", image: "KlubBodyfitFitness", address: "Plac Maksa Borna 1A, 50-001 Wrocław", desc: "gym", lat: 51.117477, lon: 17.028448, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Platinium Sports Center", image: "PlatiniumSportsCenter", address: "Kazimierska 15, 51-657 Wrocław", desc: "gym", lat: 51.104483, lon: 17.093097, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Marta Tymoszewicz - Fitness Studio", image: "MartaTymoszewiczFitnessStudio", address: "Jana Ewangelisty Purkyniego 10, 48-300 Wrocław", desc: "gym", lat: 51.110898, lon: 17.044176, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Klub Fitness Academy Wrocław Stare Miasto / Galeria ", image: "KlubFitnessAcademyWrocławStareMiastoGaleria ", address: "Galeria Dominikańska, plac Dominikański 3, 50-159 Wrocław", desc: "gym", lat: 51.108439, lon: 17.040524, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Jatomi Fitness", image: "JatomiFitness", address: "Renoma, Świdnicka 40, 50-086 Wrocław", desc: "gym", lat: 51.103582, lon: 17.031778, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Ultrafit Fitness", image: "UltrafitFitness", address: "3U, Kościuszki 117, 50-442 Wrocław", desc: "gym", lat: 51.099720, lon: 17.046158, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Klub Bodyfit Fitness & Wellness - Centrum Krzyki", image: "KlubBodyfitFitnessWellnessCentrumKrzyki", address: "Bernarda Pretficza 37, 11-400 Wrocław", desc: "gym", lat: 51.091855, lon: 17.006956, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Klub Fitness Academy Wrocław Arkady", image: "KlubFitnessAcademyWrocławStareMiastoGaleria", address: "Powstańców Śląskich 2, 53-333 Wrocław", desc: "gym", lat: 51.099323, lon: 17.028845, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Euro Mart", image: "EuroMart", address: "Boczna 12, 50-001 Wrocław", desc: "gym", lat: 51.092739, lon: 17.045323, rank: 0))
+        
+         sports.add(model: SingularObjectModel.singularObjectModel(name: "Fitness Academy Jupiter", image: "KlubFitnessAcademyWrocławStareMiastoGaleria", address: "Żegiestowska 11, 50-542 Wrocław", desc: "gym", lat: 51.082326, lon: 17.038825, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Music & Motion Pole Dance | Szarfy | Taniec na kole Wrocław", image: "MusicMotionPoleDance", address: "Nyska 59, 50-505 Wrocław", desc: "gym", lat: 51.087236, lon: 17.053927, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Wrocławski Park Wodny S.A.", image: "WroclawskiParkWodny", address: "Borowska 99, 50-558 Wrocław", desc: "gym", lat: 51.090943, lon: 17.032314, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Centrum Treningowe Spartan WCT5 Wrocław", image: "CentrumTreningoweSpartan", address: "Spiska 1, 50-562 Wrocław", desc: "gym", lat: 51.085267, lon: 17.031267, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Fitness24Seven", image: "Fitness24Seven", address: "Ślężna 130, 53-111 Wrocław", desc: "gym", lat: 51.080984, lon: 17.023525, rank: 0))
+        sports.add(model: SingularObjectModel.singularObjectModel(name: "Sport Klub Tęcza", image: "TeczaSportKlub", address: "Tęczowa 79, 11-400 Wrocław", desc: "gym", lat: 51.104922, lon: 17.010306, rank: 0))
+        
+        
+        
+        
         RealmAdapter.save(model: sports)
         
         handler()
